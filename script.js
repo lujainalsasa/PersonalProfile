8d20ae2b8459ef51dc6d795b0eb7eaeb511e8f4f
LujainAlsasa <lujainalsasa@gmail.com>
2025-04-23 23:12:55 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleInfo() {
+    const info = document.getElementById("extraInfo");
+    if (info.style.display === "none") {
+      info.style.display = "block";
+    } else {
+      info.style.display = "none";
+    }
+  }
+  
